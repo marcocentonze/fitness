6f53a4e4ded1fa65ee42f968837671792bb02437
marcocentonze <marco.centonze27@gmail.com>
2023-10-03 12:39:13 +0200

    add structure first section

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script>
-import AppHeader from './components/AppHeader.vue'
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue'
 
 export default {
     data() {
@@ -9,6 +10,7 @@ export default {
     },
     components: {
       AppHeader,
+      AppMain,
     },
 };
 
@@ -16,6 +18,7 @@ export default {
 
 <template>
   <AppHeader></AppHeader>
+  <AppMain></AppMain>
 </template>
 
 <style scoped>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,51 @@
+<script >
+
+export default {
+    name: 'AppMain',
+    data() {
+        return {
+
+        }
+    },
+    components: {
+
+    },
+};
+
+
+</script>
+
+<template>
+    <div class="container-fluid bg-image-custom">
+        <div class="container">
+
+            <div class="">
+                <!-- left -->
+                <h5>adsda</h5>
+                <h1>Take ahdadhsjadajh</h1>
+                <!-- linea gialla -->
+                <p>adadsadsad</p>
+                <span>
+                    <button>visit yt</button>
+                    <button>but it today</button>
+                </span>
+            </div>
+        </div>
+        <!-- right -->
+       
+
+
+
+    </div>
+</template>
+
+<style scoped>
+.bg-image-custom{
+    background-image: url("../assets/img/img/slider82x-scaled.jpg");
+    background-size: cover;
+    height:700px;
+    
+}
+
+
+</style>
