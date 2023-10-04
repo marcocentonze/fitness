0a7921459d654b79dac80ea82154a37251f39359
marcocentonze <marco.centonze27@gmail.com>
2023-10-04 20:50:20 +0200

    add footer component

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script>
 import AppHeader from './components/AppHeader.vue';
-import AppMain from './components/AppMain.vue'
+import AppMain from './components/AppMain.vue';
+import AppFooter from './components/AppFooter.vue';
 
 export default {
     data() {
@@ -11,6 +12,7 @@ export default {
     components: {
       AppHeader,
       AppMain,
+      AppFooter
     },
 };
 
@@ -19,6 +21,7 @@ export default {
 <template>
   <AppHeader></AppHeader>
   <AppMain></AppMain>
+  <AppFooter></AppFooter>
 </template>
 
 <style scoped>
